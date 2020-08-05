710324d0c81cf0d01fd8af9cdc493ba571a5d063
NAKAJIMA Yusaku <yupong7@gmail.com>
2020-08-06 02:50:41 +0900

    add test case.

--- test.sh
+++ test.sh
@@ -6,6 +6,9 @@ function error() {
 }
 
 cmp <(cat cat.c | ./cat) cat.c || error "Error: differ $LINENO"
+cmp <(cat /dev/null | ./cat) /dev/null || error "Error: differ $LINENO"
+
+cmp <(./cat cat.c) cat.c || error "Error: differ $LINENO"
 
 echo "Ok." 
 
