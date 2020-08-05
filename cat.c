e9347132fc0d4a8d47f09697c104c9241334ccf2
NAKAJIMA Yusaku <yupong7@gmail.com>
2020-08-06 03:07:42 +0900

    rewrite

--- cat.c
+++ cat.c
@@ -2,18 +2,12 @@
 
 int main(int argc, char **argv) {
   char **args = argv + 1;
-
-  if (argc == 1) {
-    int c;
-    while ((c = getchar()) != EOF) {
-      putchar(c);
-    }
-
-    return 0;
-  }
-
   FILE *f;
-  f = fopen(*args, "r");
+
+  f = stdin;
+  if (argc != 1) {
+    f = fopen(*args, "r");
+  }
 
   int c;
   while ((c = fgetc(f)) != EOF) {
