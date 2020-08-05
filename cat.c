ae670eaae93643c5828b6a68f91da989321fecdc
NAKAJIMA Yusaku <yupong7@gmail.com>
2020-08-06 03:04:15 +0900

    support file

--- cat.c
+++ cat.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
+  char **args = argv + 1;
+
+  if (argc == 1) {
+    int c;
+    while ((c = getchar()) != EOF) {
+      putchar(c);
+    }
+
+    return 0;
+  }
+
+  FILE *f;
+  f = fopen(*args, "r");
+
   int c;
-  while ((c = getchar()) != EOF) {
-    putchar(c);
+  while ((c = fgetc(f)) != EOF) {
+    fputc(c, stdout);
   }
 
   return 0;
